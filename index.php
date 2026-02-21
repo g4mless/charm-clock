d7ea0c5d11667d7f0cd77f115c041f7b1d26110e
g4mless <g4mless@proton.me>
2026-02-22 01:47:38 +0700

    init

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Charm Clock</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="clock-container">
+        <div class="time-section">
+            <!-- Jam besar HH:MM -->
+        </div>
+        <div class="date-section">
+            <!-- Hari, Tanggal Bulan -->
+        </div>
+    </div>
+    
+    <script src="clock.js"></script>
+</body>
+</html>
